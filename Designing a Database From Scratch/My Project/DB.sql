05a4dc388dcbc4346e8286cb2b9d92e4c71301a8
Andres Gomez <andresxv98@gmail.com>
2024-10-23 11:50:27 -0500

    DB finished

--- Designing a Database From Scratch/My Project/DB.sql
+++ Designing a Database From Scratch/My Project/DB.sql
@@ -13,7 +13,6 @@ CREATE TABLE orders (
     date date,
     status varchar(50),
     shipping_address varchar(50),
-    payment_id integer REFERENCES payments(payment_id)
 );
 
 CREATE TABLE payments (
@@ -41,4 +40,39 @@ CREATE TABLE order_items (
     quantity integer,
     price money
 );
+--Para representar una relación de muchos a muchos (Many to Many) entre las tablas Orders y Products, crear una tabla intermedia (a menudo llamada tabla de asociación o tabla de enlace) que contenga las claves primarias de ambas tablas. Permitirá que una orden contenga múltiples productos y que un producto pueda aparecer en múltiples órdenes.
 
+CREATE TABLE order_products (
+    order_id integer REFERENCES orders(order_id),
+    product_id integer REFERENCES products(product_id),
+    quantity integer,
+    PRIMARY KEY (order_id, product_id)
+);
+
+INSERT INTO customers (customer_id, first_name, last_name, email_address, phone_number, shipping_address)
+VALUES (1, 'Andrew', 'G.', 'andrewg@gmail.com', 3245768250, 'Cl 1E # 21-89');
+
+INSERT INTO products (product_id, name, price, description, availability, category)
+VALUES (1, 'Mouse', 50, 'Bluetooth Mouse', TRUE, 'Technology');
+
+INSERT INTO orders (order_id, customer_id, date, status, shipping_address, payment_id)
+VALUES (1, 1, '2024-10-23', 'Pending', 'Cl 1E # 21-89', 1);
+
+INSERT INTO payments (payment_id, order_id, method, amount, date, status)
+VALUES (1, 1, 'Credit Card', 50, '2024-10-23', 'Complete')
+
+INSERT INTO order_items (order_item_id, order_id, product_id, quantity, price)
+VALUES (1, 1, 1, 1, 50);
+
+INSERT INTO orders (order_id, customer_id, date, status, shipping_address)
+VALUES (2, 1, '2024-10-23', 'Pending', 'Cl 1E # 21-89');
+
+INSERT INTO order_items (order_item_id, order_id, product_id, quantity, price)
+VALUES (2, 2, 1, 1, 50);
+
+INSERT INTO products (product_id, name, price, description, availability, category)
+VALUES (2, 'Keyboard', 80, 'Bluetooth Keyboard', TRUE, 'Technology');
+
+INSERT INTO order_products (order_id, product_id, quantity)
+VALUES (1, 1, 2), -- 2 unidades del Producto A
+       (1, 2, 1); -- 1 unidad del Producto B
